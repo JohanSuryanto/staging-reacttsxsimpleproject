c0d8799090d88b76bf25638a64bdd075208f46e5
Johan Suryanto <johan.suryanto@digitalavenues.com>
2025-12-09 17:25:40 +0700

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -113,7 +113,7 @@ function App() {
                 </a>
               </div>
 
-              <h1>Vite + React</h1>
+              <h1>Vite + React (STAGING)</h1>
 
               <div className="card">
                 <button onClick={() => setCount((count) => count + 1)}>
